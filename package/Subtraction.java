378fb322f2f157382f8fe7a7b87ed7c6b7d204c6
abhishek <160867211+abhi15102004@users.noreply.github.com>
2024-04-25 13:13:20 +0530

    Add files via upload
    Package (ArthmeticOp)

---
+++ package/Subtraction.java
@@ -0,0 +1,12 @@
+package arithmeticop;
+public class Subtraction{
+	private int d1,d2;
+	public Subtraction(int n1,int n2){
+		this.d1=n1;
+		this.d2=n2;
+	}
+	public void difference()
+	{
+		System.out.println("sub of two given numbers is"+(d1+d2));
+	}
+}
